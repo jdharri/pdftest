82dd159442eda06c70b84011e965f259311c53cc
root <root@localhost.localdomain>
2016-12-08 13:50:44 -0500

    more stuff

--- src/main/java/com/example/pdf/pdftest/App.java
+++ src/main/java/com/example/pdf/pdftest/App.java
@@ -1,27 +1,17 @@
 package com.example.pdf.pdftest;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDNonTerminalField;
-import org.apache.pdfbox.pdmodel.interactive.form.PDXFAResource;
-import org.omg.CORBA.portable.OutputStream;
 
 /**
- * Hello world!
+ * Filling out a PDF form
  *
  */
 
@@ -34,10 +24,10 @@ public class App {
 
 	public static void loadPDF() {
 		try {
-			doc = PDDocument.load(new File("/tmp/aaa.pdf"));
+			doc = PDDocument.load(new File("/tmp/FORM4414.pdf"));
 
 			PDDocumentCatalog catalog = doc.getDocumentCatalog();
-			PDPage page1 = doc.getPage(0);
+		
 
 			PDAcroForm form = catalog.getAcroForm();
 			List<PDField> fields = form.getFields();
@@ -45,7 +35,7 @@ public class App {
 			System.out.println(fields);
 			for (PDField f : fields) {
 
-				PDField nField = list(f);
+				list(f);
 
 			}
 
@@ -64,7 +54,7 @@ public class App {
 		if (field instanceof PDNonTerminalField) {
 			PDNonTerminalField nonTerminalField = (PDNonTerminalField) field;
 			for (PDField child : nonTerminalField.getChildren()) {
-				child.setValue("Joe" + Math.random());
+				child.setValue("face"+ Math.random());
 				list(child);
 
 			}
