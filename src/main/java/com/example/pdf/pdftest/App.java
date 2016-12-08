786cbc6ad05726b0c98243d31dfca2aaf52c6270
joel harris <joel harris>
2016-12-08 15:34:53 -0500

    added a method to generate the XFA XML

--- src/main/java/com/example/pdf/pdftest/App.java
+++ src/main/java/com/example/pdf/pdftest/App.java
@@ -2,13 +2,28 @@ package com.example.pdf.pdftest;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDNonTerminalField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDXFAResource;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  * Filling out a PDF form
@@ -19,31 +34,25 @@ public class App {
 	static PDDocument doc;
 
 	public static void main(String[] args) {
-		loadPDF();
+		 loadPDF();
+		convertToXML();
 	}
 
 	public static void loadPDF() {
 		try {
-			doc = PDDocument.load(new File("/tmp/FORM4414.pdf"));
-
+			doc = PDDocument.load(new File("/temp/FORM4414.pdf"));
 			PDDocumentCatalog catalog = doc.getDocumentCatalog();
-		
-
 			PDAcroForm form = catalog.getAcroForm();
 			List<PDField> fields = form.getFields();
 
-			System.out.println(fields);
 			for (PDField f : fields) {
-
 				list(f);
-
 			}
 
-			doc.save("/tmp/aaa.pdf");
+			doc.save("/temp/aaa.pdf");
 			doc.close();
 
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 	}
@@ -51,16 +60,45 @@ public class App {
 	public static PDField list(PDField field) throws IOException {
 		System.out.println(field.getFullyQualifiedName());
 		System.out.println(field.getPartialName());
+		System.out.println(field.getValueAsString());
 		if (field instanceof PDNonTerminalField) {
 			PDNonTerminalField nonTerminalField = (PDNonTerminalField) field;
 			for (PDField child : nonTerminalField.getChildren()) {
-				child.setValue("face"+ Math.random());
+				child.setValue("face" + Math.random());
 				list(child);
-
 			}
-
 		}
-		return null;
+		return field;
+	}
+
+	public static void convertToXML() {
+		try {
+			PDDocument doc = PDDocument.load(new File("/temp/FORM4414.pdf"));
+
+			PDDocumentCatalog catalog = doc.getDocumentCatalog();
+			PDAcroForm form = catalog.getAcroForm();
+
+			PDXFAResource xfa = form.getXFA();
+
+			PrintWriter out = new PrintWriter("/temp/pdf.xml");
+			StreamResult result = new StreamResult(new StringWriter());
+
+			Document domDoc = xfa.getDocument();
+			Transformer trans = TransformerFactory.newInstance().newTransformer();
+			trans.setOutputProperty(OutputKeys.INDENT, "yes");
+			trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			DOMSource src = new DOMSource(domDoc);
+			trans.transform(src, result);
+			System.out.println(result.getWriter().toString());
+			out.println(result.getWriter().toString());
+			out.close();
+
+
+		} catch (IOException | ParserConfigurationException | TransformerException | SAXException
+				| TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
